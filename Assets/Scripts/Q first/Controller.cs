076dca394f9f4835fc2e3cbef492cdf1f2d988b0
Qwertz-0 <edy.stef@ymail.com>
2022-05-24 18:00:42 +0100

    New scene with an attempt

---
+++ Assets/Scripts/Q first/Controller.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] Transform cam;
+    [SerializeField] GameObject mainBody;
+
+
+    public float forwardSpeed = 10f;
+    public float sideSpeed = 6f;
+    public float reverseSpeed = 5f;
+
+    private float speed;
+
+    Vector3 direction;
+
+
+    private float mainBodyVelocitySmooth;
+    [SerializeField] float mainBodySmoothTime = 0.1f;
+
+    private float robotScmoothVelocity;
+    [SerializeField] float robotSmoothTime = 0.3f;
+
+
+    [SerializeField] float testCounter1;
+    [SerializeField] float testCounter2;
+    [SerializeField] float testCounter3;
+    [SerializeField] float testCounter4;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (direction.magnitude >= 0.05f)
+        {
+            float robotTargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float robotAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, robotTargetAngle, ref robotScmoothVelocity, robotSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, robotAngle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, robotAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDirection * Time.deltaTime * 10);
+        }
+
+
+        float mainBodyTargetAngle = (cam.eulerAngles.y);
+        float mainBodyAngle = Mathf.SmoothDampAngle(mainBody.transform.eulerAngles.y, mainBodyTargetAngle, ref mainBodyVelocitySmooth, mainBodySmoothTime);
+        mainBody.transform.rotation = Quaternion.Euler(0f, mainBodyAngle, 0f);
+    }
+
+    
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+
+        if (inputVector.y <= -0.05f)
+        {
+            speed = reverseSpeed;
+        }
+        else
+        {
+            speed = forwardSpeed;
+        }
+
+        direction = new Vector3(inputVector.x * sideSpeed, 0, inputVector.y * speed);
+    }
+}
